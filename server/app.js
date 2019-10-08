63bed98db96da516e02efa0a16d242cee655e5f4
Angel Manuel Goez Giraldo <angel.goez@da.arus.com.co>
2019-10-07 20:09:16 -0500

    Command line added

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const forecast = require('./utils/forecast')
 const chalk = require('chalk')
 const request = require('request')
 
+const address = process.argv[2]
+
 
 // request({ url, json:true }, (error, response) => {
 //     if(error){
@@ -11,19 +13,25 @@ const request = require('request')
 //     console.log(`Actualmente afuera está ${ chalk.bold.blue(response.body.currently.temperature)} °C y hay ${ chalk.bold.blue(response.body.currently.precipProbability)} % probabilidad que llueva`)
 // })
 
-geocode('Cañasgordas', (error, response) => {
+if (!address ){
+    console.log(chalk.red('Please provide an address'))
+} else {
+    geocode(address, (error, response) => {
 
-    if(error){
-        return console.log(error)
-    }
-    const { location, latitude, longitude } = response 
-
-    forecast(latitude, longitude, (error, response) => {
-        if (error){
-            console.log(error)
-        } else {
-            console.log(`${response} en ${location}`)
+        if(error){
+            return console.log(error)
         }
+        const { location, latitude, longitude } = response 
+    
+        forecast(latitude, longitude, (error, response) => {
+            if (error){
+                console.log(error)
+            } else {
+                console.log(`${response} en ${location}`)
+            }
+        })
     })
-})
+}
+
+
 
